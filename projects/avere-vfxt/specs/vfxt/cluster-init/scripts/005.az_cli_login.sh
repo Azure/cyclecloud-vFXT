cde00080353cef23ce20955d516a9d48b5312446
Ryan Hamel <ryan.hamel@cyclecomputing.com>
2018-09-26 12:41:00 -0400

    ensure tee fails if launch_azure.py fails

--- projects/avere-vfxt/specs/vfxt/cluster-init/scripts/005.az_cli_login.sh
+++ projects/avere-vfxt/specs/vfxt/cluster-init/scripts/005.az_cli_login.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -e
 set -ex
 rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sh -c 'echo -e "[azure-cli]\nname=Azure CLI\nbaseurl=https://packages.microsoft.com/yumrepos/azure-cli\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/azure-cli.repo'
--- projects/avere-vfxt/specs/vfxt/cluster-init/scripts/010.launch_vfxt.sh
+++ projects/avere-vfxt/specs/vfxt/cluster-init/scripts/010.launch_vfxt.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -e
 set -ex
 pushd /root
 LAUNCH=$CYCLECLOUD_SPEC_PATH/files/launch_azure.py
@@ -8,10 +8,16 @@ EX_FILE="launch_vfxt_cluster.sh"
 touch $EX_FILE
 echo "source /root/.venv/vfxt-azure/bin/activate" >> $EX_FILE
 echo "export PYTHONHTTPSVERIFY=0" >> $EX_FILE
+echo "az login -i" >> $EX_FILE
+echo "set -o pipefail" >> $EX_FILE
 echo "python launch_azure.py 2>&1 | tee vfxt_cluster_launch.log &" >> $EX_FILE
 
 # launch cluster
 cp $LAUNCH ./
 source /root/.venv/vfxt-azure/bin/activate
 export PYTHONHTTPSVERIFY=0
+
+
+# tee will exit 0  if you don't set this
+set -o pipefail
 python $LAUNCH 2>&1 | tee vfxt_cluster_launch.log
