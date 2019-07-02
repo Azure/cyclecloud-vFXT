fcd05489cbf4757bd30c90dd537de3f615c098a3
Michael Requa <mirequa@microsoft.com>
2019-07-02 15:27:46 -0700

    purge legacy

--- projects/avere-vfxt/specs/default/chef/site-cookbooks/avere-client/attributes/default.rb
+++
@@ -1,4 +0,0 @@
-default[:vfxt][:client][:nfs][:alias] = 'vfxt'
-default[:vfxt][:client][:nfs][:mountpoint] = '/mnt/vfxt'
-default[:vfxt][:client][:nfs][:fstype] = 'nfs'
-default[:vfxt][:server][:nfs][:export] = '/'
